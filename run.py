3c79efd17809a48fab5ef0496ea52bfba601e68c
chenzx <chenzx@lrwanche.com>
2020-04-15 17:47:50 +0800

    提交

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ import os
 
 if __name__ == '__main__':
     try:
-        # print(os.system('%s' % ("pip freeze > requirements.txt")))#部署需要生成requirements.txt文件（宝塔部署需要修改生成文件内的Django 改为django）
+        print(os.system('%s' % ("pip freeze > requirements.txt")))#部署需要生成requirements.txt文件（宝塔部署需要修改生成文件内的Django 改为django）
         # print(os.system('%s' % ("py manage.py makemigrations")))
         # print(os.system('%s' % ("py manage.py migrate")))
-        print(os.system('%s' % ("py manage.py runserver")))
+        # print(os.system('%s' % ("py manage.py runserver")))
         # print(os.system('%s' % ("py manage.py startapp safetest")))#创建新的APP注意修改url，settingT
     except Exception as e:
         print(e)
